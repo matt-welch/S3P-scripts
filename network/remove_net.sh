9be02902e2805d5bde287707fe8c292db366c128
Matt Welch <matt.welch@intel.com>
2016-07-18 20:45:43 -0400

    feat: add scripts for network management
    -One script setup_net.sh for settuing up bridges and adding the
    appropriate interfaces.
    -A second script remove_net.sh for tearing them down.

---
+++ network/remove_net.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# tear down the network in teh opposite order that it's setup
+# see setup_net.sh 
+
+source network-details.sh
+
+if [ -n "$(pgrep dhclient)" ] ; then 
+    # ERROR, a dhclient process is already running
+    echo "ERROR: ${0} depends on dhclient which is already running:"
+    ps -ef | grep dhclient | grep -v grep 
+    echo "Please stop the dhclient process above and try again."
+    echo
+    exit 1
+fi
+
+# remove interface then destroy tenant network bridge
+ip link set dev ${TENANT_BR} down 
+brctl delif ${TENANT_BR} ${TENANT_IF}
+brctl delbr ${TENANT_BR}
+
+# remove interface and bridge for management network
+ip link set dev ${MGMT_BR} down 
+ifconfig ${MGMT_IF} ${MGMT_SUBNET}
+brctl delif ${MGMT_BR} ${MGMT_IF}
+brctl delbr ${MGMT_BR}
+
+# remove interface and bridge for internet network
+ifconfig ${NET_BR} 0 
+ip link set dev ${NET_BR} down
+brctl delif ${NET_BR} ${NET_IF}
+brctl delbr ${NET_BR}
+ip link set dev ${NET_IF} up 
+dhclient ${NET_IF}
---
+++ network/setup_net.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# create a set of bridges for network testing
+
+source network-details.sh
+
+if [ -n "$(pgrep dhclient)" ] ; then 
+    # ERROR, a dhclient process is already running
+    echo "ERROR: ${0} depends on dhclient which is already running:"
+    ps -ef | grep dhclient | grep -v grep 
+    echo "Please stop the dhclient process above and try again."
+    echo
+    exit 1
+fi
+
+#create internet bridge and assign IP to it
+echo "Creating internet bridge ${NET_BR}..."
+brctl addbr ${NET_BR}
+brctl addif ${NET_BR} ${NET_IF}
+ifconfig ${NET_IF} 0
+ip link set dev ${NET_BR} up
+dhclient ${NET_BR}
+
+# create management bridge and assign IP
+echo "Creating ngmt network bridge ${NET_BR}..."
+brctl addbr ${MGMT_BR}
+brctl addif ${MGMT_BR} ${MGMT_IF}
+ifconfig ${MGMT_IF} 0
+ip link set dev ${MGMT_BR} up 
+ip a a ${MGMT_SUBNET} dev ${MGMT_BR} 
+
+# create tenant network bridge and assign interface
+echo "Creating tenant network bridge ${NET_BR}..."
+brctl addbr ${TENANT_BR}
+brctl addif ${TENANT_BR} ${TENANT_IF}
+ifconfig ${TENANT_IF} 0
+ip link set dev ${TENANT_BR} up 
+
+echo "S3P network creation complete."
+ip a s ${NET_BR}
+ip a s ${MGMT_BR}
+ip a s ${TENANT_BR}
+brctl show 
