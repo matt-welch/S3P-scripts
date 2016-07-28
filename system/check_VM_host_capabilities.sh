e811f1598061f90787663c157388b8191553120d
Matt Welch <matt.welch@intel.com>
2016-07-28 15:49:10 -0400

    feat: improve color support for system status
    Add bash_colors.sh script
    Enable color output in sys check
    Ignore .help files
    
    bash_colors.sh courtesy of https://github.com/matt-welch/GENI_VT.git

--- system/check_VM_host_capabilities.sh
+++ system/check_VM_host_capabilities.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Checking for VMX/SVM support in the CPU (output=positive): "
-grep -E "vmx|smv" /proc/cpuinfo
+grep -E "vmx|smv" /proc/cpuinfo --color
 
 echo 
 echo "Checking for DMAR and IOMMU (output=positive): "
-dmesg | grep -e DMAR -e IOMMU
+dmesg | grep -e DMAR -e IOMMU --color
 
